62a96aa199eed4cb7d01164f5b13193a0b5e343d
Marcus Sanches <marcus_sanchesant@hotmail.com>
2022-01-20 16:17:01 -0300

    Criacao da logica para carregar arquivos xml, ler e converter para dados de model

--- src/app/arquivo-xml/arquivo-xml.component.ts
+++ src/app/arquivo-xml/arquivo-xml.component.ts
@@ -1,5 +1,7 @@
+import { ArquivoXmlService } from './arquivo-xml.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import * as xml2js from 'xml2js';
 
 @Component({
   selector: 'app-arquivo-xml',
@@ -7,7 +9,39 @@ import { Router } from '@angular/router';
   styleUrls: ['./arquivo-xml.component.css'],
 })
 export class ArquivoXmlComponent implements OnInit {
-  constructor(private router: Router) {}
 
-  ngOnInit(): void {}
+  constructor(private router: Router, public arquivoXmlService: ArquivoXmlService) { }
+
+  ngOnInit(): void { }
+
+  selecionarArquivos(event: any) {
+    for (let arquivo of event.files) {
+      this.leitorArquivosXml(arquivo);
+    }
+  }
+
+  leitorArquivosXml(arquivo: any) {
+    const reader = new FileReader();
+    reader.readAsText(arquivo);
+    reader.onload = (evt) => {
+      const xml: string = (evt as any).target.result;
+      this.converterXmlJson(xml)
+    };
+  }
+
+  converterXmlJson(xml: string) {
+    const conversor: xml2js.Parser = new xml2js.Parser({explicitArray:false});
+    conversor.parseString(xml, (err: any, result: any) => {
+      if (err) {
+        console.error(err)
+        throw err;
+      }
+      const json = JSON.stringify(result, null, 4);
+    });
+  }
+
+  adicionarArquivoLista(json: any) {
+    const arquivo = JSON.parse(json);
+    this.arquivoXmlService.arquivos.push(arquivo);
+  }
 }
--- src/app/arquivo-xml/arquivo-xml.module.ts
+++ src/app/arquivo-xml/arquivo-xml.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArquivoXmlComponent } from './arquivo-xml.component';
 import { DuplicatasComponent } from './duplicatas/duplicatas.component';
+import { FileUploadModule } from 'primeng/fileupload';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [ArquivoXmlComponent, DuplicatasComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FileUploadModule, HttpClientModule],
   exports: [ArquivoXmlComponent, DuplicatasComponent],
 })
 export class ArquivoXmlModule {}
